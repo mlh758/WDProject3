f59b233f81993cbc4508c09810127934c8c4aff8
Michael <osiris2918@gmail.com>
2015-11-23 12:33:40 -0600

    Added Search
    Currently unable to test without coming to campus.

---
+++ php/search.php
@@ -0,0 +1,55 @@
+<?php
+
+require_once 'connection.php';
+    $data = $_POST['search'];
+	$terms = explode(" ", $data);
+	
+    $query = "SELECT c.ID, c.`Picture`, c.`Picture_type`, c.`Color`, cs.`Make`, cs.`Model`, cs.`YearMade`, cs.`Size` ";
+    $query .= "FROM Car c, CarSpecs cs WHERE c.`CarSpecsID` = cs.`ID` AND c.`Status` = 1";
+	if(count($terms) > 0){
+		$clause = " AND (";
+		$count = 0;
+		foreach($terms as $item){
+			if($count > 0){
+				$clause .= " OR ";
+			}
+			$value = (int) $item;
+			if($value > 0){
+				$clause .= "cs.`YearMade` = $item";
+			}
+			else{
+				$clause .= "c.`Color` LIKE '%$item'";
+				$clause .= "cs.`Make` LIKE '$item'";
+				$clause .= "cs.`Model` LIKE '$item'";
+				$clause .= "cs.`Size` LIKE '$item'";
+			}
+			$count += 1;
+		}
+		$clause .= ");";
+	}
+	
+    $result = mysqli_query($connection, $query);
+
+    if (!$result)
+        die("Database access failed: " . mysqli_error($connection));
+
+
+    $cars = build_car_array($result);
+    mysqli_close($connection);
+    echo json_encode($cars);
+
+
+//make JSON for cars
+function build_car_array($results) {
+	$final_result = array();
+	$row_count = mysqli_num_rows($results);
+	for($i=0;$i<$row_count;++$i){
+		$row = mysqli_fetch_array($results);
+		$image = 'data:' . $row["Picture_Type"] . ';base64,' . base64_encode($row["Picture"]);
+		$item = array("make"=>$row["Make"],"model"=>$row["Model"], "year"=>$row["YearMade"], "color"=>$row["Color"], "ID"=>$row["ID"], "picture"=>$image);
+		$final_result["items"][]=$item;
+	}
+	return $final_result;
+}
+
+?>
--- script/cars.js
+++ script/cars.js
@@ -1,5 +1,24 @@
 $(document).ready(init);
 
 function init() {
+	$("#find-car").click()
+}
 
+function findCars(){
+	var text = $("#find-car-input").val();
+	$.ajax({
+	  type: "POST",
+	  url: "server/search.php",
+	  data: {search: text},
+	  success: function(data){
+		  renderCars(data);
+	  },
+	  dataType: "json"
+	});
+}
+
+function renderCars(cars){
+	var searchTemplate = $("#find-car-template").html();
+	var searchMaker = new htmlMaker(searchTemplate);
+	$("#search_results").html(searchMaker.getHTML(cars));
 }
